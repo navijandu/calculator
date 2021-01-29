9cf79058784a16c5bea19903e00c83721e129c60
Ashmeet Singh <ashmeetsingh2010@gmail.com>
2021-01-29 16:35:45 -0500

    audio

--- Extension/js/main.js
+++ Extension/js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
+    /*   var audio = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/click.mp3');*/
+    var audio2 = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/clickUp.mp3')
 
+    $(".btn").mousedown(function () {
+        audio2.load();
+        audio2.play();
+    });
+
+    /*  $(".btn").mouseup(function () {
+        audio.load();
+        audio.play();
+    });*/
 
 });
 
